0f91ede3a952c4033d34a08294d796fb821ec818
Jorge Freitas <80436467+jorgelcff@users.noreply.github.com>
2024-04-08 18:18:45 -0300

    [UPD] Fixed Mutiply Items

--- server.py
+++ server.py
@@ -10,22 +10,34 @@ def make_predictions(data, periods, granularidade, start_date):
     df = pd.DataFrame(data)
     df['timestamp'] = pd.to_datetime(df['timestamp'])
     df.set_index('timestamp', inplace=True)
+
+
     freq_map = {'D': 'D', 'S': 'W-MON', 'M': 'ME'}
     freq_number = {'D': 30, 'S': 7, 'M': 12}
-    if len(data) < 6:
-        model = ARIMA(df['demand'], order=(1, 1, 1))
-        model_fit = model.fit()
-    else:
-        model = SARIMAX(df['demand'], order=(1, 1, 1), seasonal_order=(1, 1, 1, freq_number[granularidade]))
-        model_fit = model.fit(disp=0)
+    df_sales = pd.json_normalize(data)
+    df_sales['timestamp'] = pd.to_datetime(df_sales['timestamp'])
+    df_sales_grouped = df_sales.groupby(['item_id', pd.Grouper(key='timestamp', freq=freq_map[granularidade])])[
+        'demand'].sum().reset_index()
 
-    future_dates = pd.date_range(start=start_date, periods=periods, freq=freq_map[granularidade])
-    future_dates_str = [date.strftime("%Y-%m-%d") for date in future_dates]
+    df_previsoes = pd.DataFrame(columns=['mercadoria_id', 'dPrevisao', 'periodo', 'qTotal'])
 
-    forecast = model_fit.forecast(steps=periods)
+    for produto in df_sales_grouped['item_id'].unique():
+        df_produto = df_sales_grouped[df_sales_grouped['item_id'] == produto]
 
-    return [{"dPrevisao": date, "mercadoria_id": data[0]["item_id"], "periodo": "D", "qTotal": int(prediction)} for
-            date, prediction in zip(future_dates_str, forecast)]
+        if len(data) < 6:
+            model = ARIMA(df_produto['demand'], order=(1, 1, 1))
+            model_fit = model.fit()
+        else:
+            model = SARIMAX(df_produto['demand'], order=(1, 1, 1), seasonal_order=(1, 1, 1, freq_number[granularidade]))
+            model_fit = model.fit(disp=0)
+
+        date_range = pd.date_range(start=start_date, periods=periods, freq=freq_map[granularidade])
+
+        forecast = model_fit.forecast(steps=periods)
+        for date, vendas in zip(date_range, forecast):
+            formatted_date = date.strftime("%Y-%m-%d")
+            df_previsoes.loc[len(df_previsoes)] = [produto, formatted_date, granularidade, round(vendas)]
+    return jsonify(df_previsoes.to_dict(orient='records'))
 
 @app.route('/forecast', methods=['POST'])
 def predict():
@@ -42,7 +54,7 @@ def predict():
 
     predictions = make_predictions(sales_data, periodos, granularidade, start_date)
 
-    return jsonify(predictions)
+    return predictions
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=4000, debug=True)
